049309db6e399e242c808ac49e48f552c2859f58
Azurly <azerly.hn@gmail.com>
2023-11-15 08:50:30 +0100

    commit

--- src/Controller/admin/adminFormationsController.php
+++ src/Controller/admin/adminFormationsController.php
@@ -1,3 +1,47 @@
 <?php
 namespace App\Controller\admin;
-?>
+
+use App\Repository\CategorieRepository;
+use App\Repository\FormationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PropertyInfo\Type;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controleur des formations
+ *
+ * @author emds
+ */
+class adminFormationsController extends AbstractController {
+
+    const CHEMINADMINFORMATIONHTMLTWIG = "admin/admin.formations.html.twig";
+    /**
+     * @var FormationRepository
+     */
+    private $formationRepository;
+    
+    /**
+     * @var CategorieRepository
+     */
+    private $categorieRepository;
+    
+    public function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
+        $this->formationRepository = $formationRepository;
+        $this->categorieRepository= $categorieRepository;
+    }
+    
+    /**
+     * @Route("/admin/formations", name="admin.formations")
+     * @return Response
+     */
+    public function index(): Response{
+        $formations = $this->formationRepository->findAll();
+        $categories = $this->categorieRepository->findAll();
+        return $this->render(self::CHEMINADMINFORMATIONHTMLTWIG, [
+            'formations' => $formations,
+            'categories' => $categories
+        ]);
+    }
+}
