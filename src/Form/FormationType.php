317d8c0f63e50f3a0f78cdd6d6870d0b470802ec
Azurly <azerly.hn@gmail.com>
2023-11-21 00:18:21 +0100

    commit

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -2,23 +2,33 @@
 
 namespace App\Form;
 
+use App\Entity\Categorie;
 use App\Entity\Formation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class FormationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('publishedAt', null, [
+            ->add('publishedAt', DateType::class, [
                 'widget' => 'single_text',
                 'label' => 'date'
                 ])
             ->add('title')
+            ->add('description')
             ->add('videoId')
+            ->add('categories', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => false
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
