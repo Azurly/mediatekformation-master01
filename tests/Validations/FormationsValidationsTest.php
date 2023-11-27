80277ea568cf8bb3103a56b0d0e64677d6ff13ed
Azurly <azerly.hn@gmail.com>
2023-11-27 15:37:23 +0100

    commit

--- tests/Validations/FormationsValidationsTest.php
+++ tests/Validations/FormationsValidationsTest.php
@@ -1,25 +1,35 @@
 <?php
 
-namespace App\Tests\Validation;
+namespace App\Tests\Validations;
 use App\Entity\Formation;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class FormationsValidationsTest extends KernelTestCase{
 
+        // 1 : Date au format string
+        public function testDateFormatString(){
+            $formation = new Formation();
+            $formation->setPublishedAt(new \DateTime("2022-04-14"));
+            $this->assertEquals("14/04/2022", $formation->getPublishedAtString());
+        }
+
+        //Instanciation d'une formation pour les tests
         public function getFormation(): Formation{
             return(new Formation())->setTitle("Cours n°5555 sur les Test Kernel")->setVideoId("vefvVEv56");
         }
 
-        public function testDateInferieurAAujourdhui(){
-            $formation = $this->getFormation()->setPublishedAt(new \DateTime('25/12/2025'));
+        //TestDate inferieur à aujourd'hui
+        public function testDateInferieurEgalToday(){
+            $formation = $this->getFormation()->setPublishedAt(new \DateTime("2024-04-04"));
             $this->assertErrors($formation, 1);
-        }
 
-        public function assertErrors(Formation $formation, int $nbErreursAttendues){
+        }
+        //Fonction qui sert à gérer les erreurs
+        public function assertErrors(Formation $formation, int $nbErreursAttendues, string $message=""){
             self::bootKernel();
             $validator = self::getContainer()->get(ValidatorInterface::class);
             $error = $validator->validate($formation);
-            $this->assertCount($nbErreursAttendues, $error);
+            $this->assertCount($nbErreursAttendues, $error, $message);
         }
 }
