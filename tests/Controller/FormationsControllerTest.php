b2ac43fd1476139f86a4ee05d4a2cdaf56a365d3
Azurly <azerly.hn@gmail.com>
2023-12-08 18:37:59 +0100

    commit

--- tests/Controller/FormationsControllerTest.php
+++ tests/Controller/FormationsControllerTest.php
@@ -12,13 +12,23 @@ class FormationsControllerTest extends WebTestCase{
         $client->request('GET', '/formations');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
-    public function testLinkFormation(){
+    public function testFiltreFormations(){
         $client = static::createClient();
         $client->request('GET', '/formations');
-        $client->clickLink('Z4yTTXka958');
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Eclipse n°8 : Déploiement'
+        ]);
+        $this->assertCount(1, $crawler->filter('h5'));
+        $this->assertSelectorTextContains('h5', 'Eclipse n°8 : Déploiement');
+    }
+
+    public function testLinkFormations(){
+        $client = static::createClient();
+        $client->request('GET', '/formations');
+        $client->clickLink('Les miniatures formations');
         $response = $client->getResponse();
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
-        $uri = $client->getRequest()->server->get("REQUEST_URL");
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
         $this->assertEquals('/formations/formation/1', $uri);
     }
 }
