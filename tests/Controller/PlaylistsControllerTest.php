1e3fddc176f79b3dc783e00cac7cea4e253b38a5
Azurly <azerly.hn@gmail.com>
2023-12-08 19:10:45 +0100

    commit

--- tests/Controller/PlaylistsControllerTest.php
+++ tests/Controller/PlaylistsControllerTest.php
@@ -12,4 +12,23 @@ class PlaylistsControllerTest extends WebTestCase{
         $client->request('GET', '/playlists');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
+    public function testFiltrePlaylists(){
+        $client = static::createClient();
+        $client->request('GET', '/playlists');
+        $crawler = $client->submitForm('filtrer', [
+            'recherche' => 'Bases de la programmation (C#)'
+        ]);
+        $this->assertCount(1, $crawler->filter('h5'));
+        $this->assertSelectorTextContains('h5', 'Bases de la programmation (C#)');
+    }
+
+    public function testLinkPlaylists(){
+        $client = static::createClient();
+        $client->request('GET', '/playlists');
+        $client->clickLink('Voir détail');
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $uri = $client->getRequest()->server->get("REQUEST_URI");
+        $this->assertEquals('/playlists/playlist/13', $uri);
+    }
 }
